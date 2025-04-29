b1dfc304b7e648fa6f4d85942ca1838ff805624a
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-04-29 12:49:25 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Login_asp.net_web_app._Default" %>
+
+
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login Form</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Login</h2>
+            <asp:Label ID="lblUsername" runat="server" Text="Username:"></asp:Label>
+            <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="lblPassword" runat="server" Text="Password:"></asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+            <br />
+            <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Login_asp.net_web_app
+{
+    public partial class _Default : Page
+    {
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Here you would typically validate against a database
+            if (ValidateUser(username, password))
+            {
+                // Redirect to another page on successful login
+                Response.Redirect("Home.aspx");
+            }
+            else
+            {
+                lblMessage.Text = "Invalid username or password.";
+            }
+        }
+
+        private bool ValidateUser(string username, string password)
+        {
+            // Replace with your actual user validation logic
+            return username == "admin" && password == "password"; // Example credentials
+        }
+    }
+}
